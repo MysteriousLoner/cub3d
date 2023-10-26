7d2516be094d10feb911c98453962db0c7c029bf
MysteriousLoner <leeyang031110@gmail.com>
2023-10-26 14:30:51 +0800

    touch up

--- for_real_engine/render_minimap.c
+++ for_real_engine/render_minimap.c
@@ -8,6 +8,10 @@
 void	render_minimap(int width, int height, t_cub3d *vars, t_player *player)
 {
 	printf("\nrendering minimap\n");
+	printf("width: %d\n", width);
+	printf("height: %d\n", height);
+	printf("player x: %f\n", player->x);
+	printf("player y: %f\n", player->y);
 	(void)width;
 	(void)height;
 	(void)vars;
